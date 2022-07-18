56daa4bc697ea89fc88463285b5a4644a3f6ac24
Evgeniy Skakovskiy <Skakovsku@yandex.ru>
2022-07-18 11:15:37 +0300

    Add get_user_info
    version_18_07_22

---
+++ weather/weathers_content/get_user_info.py
@@ -0,0 +1,21 @@
+import time
+from user_agents import parse
+from .get_ip import get_client_ip
+
+def get_user_info(request, view, town):
+	ua_string = request.META['HTTP_USER_AGENT']
+	user_agent = parse(ua_string)
+	if user_agent.is_bot:
+		msg = 'Посещение бота'
+	if user_agent.is_pc:
+		msg = 'Посещение с ПК'
+	if user_agent.is_tablet:
+		msg = 'Посещение с планшета'
+	if user_agent.is_mobile:
+		msg = 'Посещение с мобильного'
+	time_log = time.ctime(time.time() + 10800)
+	log_file = open('log_user.txt', 'a')
+	ip_client = get_client_ip(request)
+	msg_client = time_log + ': ' + msg + ', ' + view + ', ' + town + ', ' + ip_client + '\n'
+	log_file.write(msg_client)
+	log_file.close()
