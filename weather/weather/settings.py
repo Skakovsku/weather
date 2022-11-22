44a5006c5a1dc1db299dc32d6c5764be74f95210
Eugenus <skakovsku@yandex.ru>
2022-11-22 19:04:07 +0300

    Pause

--- weather/weather/settings.py
+++ weather/weather/settings.py
@@ -111,8 +111,8 @@ REST_FRAMEWORK = {
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
