00e89d37f2b71b6c6a5c02aa865ddca7d9cb860f
Evgeny <Skakovsku@yandex.ru>
2023-01-05 12:37:39 +0300

    Added FILE_DATA_IP.TXT

--- weather/weathers_content/get_town.py
+++ weather/weathers_content/get_town.py
@@ -5,6 +5,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_town(ip_user):
+    if os.path.exists('./file_data_ip.txt'):
+        file = open('./file_data_ip.txt', 'r')
+        while True:
+            ip = file.readline().rstrip().split(' ')
+            if ip == ['']:
+                break
+            if ip[0] == ip_user:
+                user_town = ip[1]
+                file.close()
+                return ip[1]
+        file.close()
     token = os.getenv('token_town')
     dadata = Dadata(token)
     result = dadata.iplocate(ip_user)
@@ -13,4 +24,7 @@ def get_town(ip_user):
     user_town = result['data']['city']
     if user_town is None:
         return 'Москва'
+    file = open('./file_data_ip.txt', 'a')
+    file.write(ip_user + ' ' + user_town + '\n')
+    file.close()
     return user_town
