d79428b9358e0e25adf52bf8794283b170387198
eugenus <skakovsku@yandex.ru>
2024-05-12 14:17:27 +0300

    Add WWWCert

--- weather/weathers_content/urls.py
+++ weather/weathers_content/urls.py
@@ -6,10 +6,6 @@ app_name = 'weather'
 
 urlpatterns = [
     path(
-        '.well-known/acme-challenge/rhhk8QYgMTzkxBThHLnrf_KGvj1O2_f7VZ0dWEp8V2g/',
-        views.change_cert,
-        name='change_cert'),
-    path(
         'add-town/<str:text>/<str:town_current>/',
         views.add_town,
         name='add_town'),
